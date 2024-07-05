fc7b238607fead4a5b1018127171329557373ae7
Ana Monsalve <91292255+amonsalv@users.noreply.github.com>
2024-07-05 16:54:29 -0500

    chore: Add lazy loading for images in usePokemon hook and PokemonCard component

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,1 +1,28 @@
-export const usePokemon = () => {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IPokemon } from "../interfaces/interfaces";
+
+// exportamos la funcion usePokemon que recibe un string opcional url y un string opcional id
+export const usePokemon = (url?: string, id?: string) => { 
+  const [pokemon, setPokemon] = useState<null | undefined | IPokemon>();
+
+    // exportamos la funcion fetchPokemon que es asincrona
+  const fetchPokemon = async () => {
+    if (url) {
+      const { data }: any = await axios.get(url);
+      setPokemon(data);
+    } else if (id) {
+      const { data }: any = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      setPokemon(data);
+    }
+  };
+
+  // exportamos la funcion useEffect que recibe una funcion anonima y un array vacio, esta funcion se ejecuta cuando el componente se monta
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  return { pokemon };
+};
